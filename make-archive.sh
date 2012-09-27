ec4bf03d1f272c8533f98ce57b870543ece0fa8e
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2012-09-27 23:29:46 +0200

    archive

---
+++ make-archive.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+FILES="LICENSE.txt AUTHORS VERSION src/Makefile src/Utils.ml src/Lexer.mll src/Parser.mly src/Syntax.ml src/Normalize.ml src/Minim.ml src/Semop.ml src/Control.ml src/Pave.ml src/NormTests.ml src/STests.ml src/examples/*.ccs"
+
+DATE=$(date "+%d-%m-%Y")
+echo "v.${DATE}" > ./VERSION
+
+echo "Make archive: pave-${DATE}.tar.gz"
+tar cvzf "pave-${DATE}.tar.gz" $FILES
+
+
+
+
