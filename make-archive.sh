2e657db99658d508a987136b179a459475ba8404
Frederic Peschanski <Frederic.Peschanski@lip6.fr>
2012-10-01 23:12:31 +0200

    released

--- make-archive.sh
+++ make-archive.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-FILES="LICENSE.txt AUTHORS VERSION src/Makefile src/Utils.ml src/Lexer.mll src/Parser.mly src/Syntax.ml src/Normalize.ml src/Minim.ml src/Semop.ml src/Control.ml src/Pave.ml src/NormTests.ml src/STests.ml src/examples/*.ccs"
+FILES="LICENSE.txt AUTHORS VERSION src/Makefile src/Utils.ml src/Lexer.mll src/Parser.mly src/Syntax.ml src/Normalize.ml src/Minim.ml src/Semop.ml src/Control.ml src/Presyntax.ml src/Pave.ml src/NormTests.ml src/STests.ml src/examples/*.ccs"
 
 DATE=$(date "+%d-%m-%Y")
 echo "v.${DATE}" > ./VERSION
